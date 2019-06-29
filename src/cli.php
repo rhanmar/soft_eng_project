3d20d4852b84420dd0309905fb1ad99293f03162
Dmitriy <dimontura@yandex.ru>
2019-06-29 15:38:56 +0700

    step 3

--- src/cli.php
+++ src/cli.php
@@ -6,7 +6,7 @@ use function \cli\line;
 
 function run()
 {
-line('Welcome to the Brain Games!');
-$name = \cli\prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Games!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
